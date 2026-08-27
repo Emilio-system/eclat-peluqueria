32996edbd6ed580ff59b9c9428e570cc817f7164
Emilio <71795608+Emilio-system@users.noreply.github.com>
2026-08-26 22:56:08 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,14 @@ import App from './App.tsx'
 import './App.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container) {
+  createRoot(container).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+} else {
+  console.error("No se encontró el elemento con id 'root' en el DOM.")
+}
